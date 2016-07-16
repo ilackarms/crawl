aad614c40fb048fca0a728c8f1bd357b96780cb8
Scott Weiss <scott.weiss@emc.com>
2016-07-16 12:28:46 -0400

    player rep has limited functionality (only used to check collisions/as a placeholder for real player position)
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- game/player_rep.go
+++ game/player_rep.go
@@ -2,66 +2,42 @@ package game
 
 import tl "github.com/ilackarms/termloop"
 
-type Player struct {
+//player rep is the server's representation of the player.
+//its position is meant to be updated through input commands sent to the server
+//it should not be drawn and should not do anything on a tick
+type PlayerRep struct {
 	entity *tl.Entity
 	prevX  int
 	prevY  int
-	level  *tl.BaseLevel
-	text *tl.Text
 }
 
-// Here, Draw simply tells the Entity ent to handle its own drawing.
-// We don't need to do anything.
-func (player *Player) Draw(screen *tl.Screen) {
-	screenWidth, screenHeight := screen.Size()
-	x, y := player.entity.Position()
-	player.level.SetOffset(screenWidth / 2 - x, screenHeight / 2 - y)
-	player.text.SetPosition(x - len(player.text.Text())/2, y - 1 + screenHeight/2)
-	player.entity.Draw(screen)
-}
-
-func (player *Player) Tick(event tl.Event) {
-	player.prevX, player.prevY = player.entity.Position()
-	if event.Type == tl.EventKey {
-		// Is it a keyboard event?
-		x, y := player.entity.Position()
-		currentText := player.text.Text()
-		switch event.Key { // If so, switch on the pressed key.
-		case tl.KeyArrowRight:
-			player.entity.SetPosition(x + 1, y)
-		case tl.KeyArrowLeft:
-			player.entity.SetPosition(x - 1, y)
-		case tl.KeyArrowUp:
-			player.entity.SetPosition(x, y - 1)
-		case tl.KeyArrowDown:
-			player.entity.SetPosition(x, y + 1)
-		case tl.KeyEnter:
-			player.text.SetText("input: ")
-		case tl.KeySpace:
-			player.text.SetText(currentText+" ")
-		case tl.KeyBackspace:
-			fallthrough
-		case tl.KeyBackspace2:
-			if len(currentText) <= len("input: ") {
-				player.text.SetText("input: ")
-			} else {
-				player.text.SetText(currentText[:len(currentText)-1])
-			}
-		default:
-			player.text.SetText(player.text.Text()+string(event.Ch))
-		}
+func NewPlayerRep(entity *tl.Entity) *PlayerRep {
+	return &PlayerRep{
+		entity: entity,
 	}
 }
 
-func (player *Player) Size() (int, int) {
+func (player *PlayerRep) GetUUID() string {
+	return player.entity.GetUUID()
+}
+
+func (player *PlayerRep) Draw(screen *tl.Screen) {
+	//don't draw on server side
+}
+
+func (player *PlayerRep) Tick(event tl.Event) {
+	player.prevX, player.prevY = player.entity.Position()
+}
+
+func (player *PlayerRep) Size() (int, int) {
 	return player.entity.Size()
 }
 
-func (player *Player) Position() (int, int) {
+func (player *PlayerRep) Position() (int, int) {
 	return player.entity.Position()
 }
 
-func (player *Player) Collide(collision tl.Physical) {
+func (player *PlayerRep) Collide(collision tl.Physical) {
 	// Check if it's a Rectangle we're colliding with
 	if _, ok := collision.(*tl.Rectangle); ok {
 		player.entity.SetPosition(player.prevX, player.prevY)
