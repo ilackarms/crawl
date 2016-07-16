abde7dab2371b760113b265cec0ad932ed429994
Scott Weiss <scott.weiss@emc.com>
2016-07-15 22:02:18 -0400

    ways to serialize and deserialze level state
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ game/messages.go
@@ -0,0 +1,35 @@
+package game
+
+import tl "github.com/ilackarms/termloop"
+
+type MessageType byte
+
+const (
+	Login = MessageType(0)
+	Input = MessageType(1)
+)
+
+type LoginMessage struct {
+	Name string `json:"Name"`
+}
+
+type InputMessage struct {
+	Event tl.Event `json:"Event"`
+}
+
+type LevelChangeMessage struct {
+	LevelData levelData `json:"LevelData"`
+}
+
+type levelData struct {
+	UUID string `json:"UUID"`
+	Drawables map[string]drawableData `json:"Drawables"`
+	Bg       tl.Cell `json:"Bg"`
+	Offsetx  int `json:"Offsetx"`
+	Offsety  int `json:"Offsety"`
+}
+
+type drawableData struct {
+	Type tl.DrawableType `json:"Type"`
+	Data []byte `json:"Data"`
+}
---
+++ game/parse.go
@@ -0,0 +1,80 @@
+package game
+
+import (
+	tl "github.com/ilackarms/termloop"
+	"encoding/json"
+	"github.com/emc-advanced-dev/pkg/errors"
+)
+
+func SerializeLevel(level tl.BaseLevel) ([]byte, error) {
+	drawables := make([]drawableData, len(level.Entities))
+	for _, entity := range level.Entities {
+		var drawable drawableData
+		switch entity.(type) {
+		case tl.Entity:
+			drawable.Type = tl.DrawableType_Entity
+		case tl.Text:
+			drawable.Type = tl.DrawableType_Text
+		case tl.Rectangle:
+			drawable.Type = tl.DrawableType_Rectangle
+		default:
+			drawable.Type = tl.DrawableType_Custom
+		}
+		data, err := json.Marshal(entity)
+		if err != nil {
+			return nil, errors.New("could not convert entity to json", err)
+		}
+		drawable.Data = data
+		drawables = append(drawables, drawable)
+	}
+	ld := levelData{
+		UUID: level.UUID,
+		Drawables: drawables,
+		Bg: level.Bg,
+		Offsetx: level.Offsetx,
+		Offsety: level.Offsety,
+	}
+	data, err := json.Marshal(ld)
+	if err != nil {
+		return nil, errors.New("converting level data to json", err)
+	}
+	return data, nil
+}
+
+func DeserializeLevel(data []byte) (tl.Level, error) {
+	var ld levelData
+	if err := json.Unmarshal(data, &ld); err != nil {
+		return nil, errors.New("unmarshalling "+string(data)+" to level data", err)
+	}
+	level := tl.NewBaseLevel(ld.Bg)
+	level.Offsetx = ld.Offsetx
+	level.Offsety = ld.Offsety
+	level.UUID = ld.UUID
+	for _, drawable := range ld.Drawables {
+		var entity tl.Drawable
+		switch drawable.Type {
+		case tl.DrawableType_Entity:
+			var e tl.Entity
+			if err := json.Unmarshal(drawable.Data, &e); err != nil {
+				return nil, errors.New("unmarshalling "+string(drawable.Data)+" to entity", err)
+			}
+			entity = e
+		case tl.DrawableType_Rectangle:
+			var r tl.Rectangle
+			if err := json.Unmarshal(drawable.Data, &r); err != nil {
+				return nil, errors.New("unmarshalling "+string(drawable.Data)+" to rectangle", err)
+			}
+			entity = r
+		case tl.DrawableType_Text:
+			var t tl.Text
+			if err := json.Unmarshal(drawable.Data, &t); err != nil {
+				return nil, errors.New("unmarshalling "+string(drawable.Data)+" to text", err)
+			}
+			entity = t
+		case tl.DrawableType_Custom:
+			return nil, errors.New("unsupported drawable type: "+string(drawable.Data), nil)
+		}
+		level.AddEntity(entity)
+	}
+	return level, nil
+}
